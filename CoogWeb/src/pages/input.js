2b786b285f42dfc5aef88f84c5e433fea568209b
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-03-11 19:28:08 -0500

    Profile Header

--- CoogWeb/src/pages/input.js
+++ CoogWeb/src/pages/input.js
@@ -2,4 +2,14 @@ import React, {useState} from 'react';
 import purple_image from './purple_image.png';
 import './input.css';
 
-export const Profile = () => <div>Profile</div>;
+export const Profile = () => {
+
+    return (
+        <div className="profile-section">
+            <div className="profile-header">
+                <img src={purple_image} alt="Profile" className="profile-image" />
+                <h2 className="profile-username">Dom...urmom</h2>
+            </div>
+        </div>
+    );
+};
