bad16af40da1070a774836f6616fa54c04bb52d2
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-03-12 19:38:38 -0500

    Top Trending Header

--- CoogWeb/src/pages/wrap.js
+++ CoogWeb/src/pages/wrap.js
@@ -2,5 +2,13 @@ import React, {useState} from 'react';
 import purple_image from './purple_image.png';
 import './wrap.css';
 
-export const TopTrending = () => <div>Top Trending</div>;
+export const TopTrending = () => {
+    return (
+        <div className="trending-section">
+            <div className="trending-header">
+                <h2>Top Trending</h2>
+            </div>
+        </div>
+    );
+};
 export const CougarWrapUp = () => <div>Cougar Wrap-Up</div>;
