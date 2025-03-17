872dd1cf251eec324f7e567399596d013f2d032c
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-03-17 13:31:49 -0500

    Added all Insert Functions

--- CoogWebBack/queries.js
+++ CoogWebBack/queries.js
@@ -36,3 +36,26 @@ const getUserListOutput =
     `SELECT USER.username, USER.image_url
      FROM USER;`
 
+
+const insertAlbum = `INSERT INTO album (name, artist_id, genre, image_url, created_at) values (?,?,?,?,?);`
+
+const insertArtist = `INSERT INTO artist (username, email, password, genre, image_url, created_at) values (?,?,?,?,?,?);`
+
+const insertUser = `INSERT INTO user (username, password, email, image_url, created_at) values (?,?,?,?,?);`
+
+const insertAdmin = `INSERT INTO admin (username, password, email, image_url, created_at) values (?,?,?,?,?);`
+
+const insertFollowing = `INSERT INTO following (user_id, artist_id, followed_at) values (?,?,?);`
+
+const insertFriend = `INSERT INTO friend (user_id_1, user_id_2, friend_at) values (?,?,?);`
+
+const insertLikedAlbum = `INSERT INTO liked_album (user_id, album_id, liked_at) values (?,?,?)`
+
+const insertSong = `INSERT INTO song (name, artist_id, album_id, genre, image_url, length, song_url, created_at)
+    values (?,?,?,?,?,?,?,?);`
+
+const insertHistory = `INSERT INTO history (user_id, song_id, last_listen) values (?,?,?);`
+
+const insertLikedSong = `INSERT INTO liked_song (user_id, song_id, liked_at) values (?,?,?);`   
+
+const insertPlaylist = `INSERT INTO playlist (name, user_id, image_url, created_at) values (?,?,?,?);`
