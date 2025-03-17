c75960547e456b4d61ab6831c327eb7e515e6312
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-03-16 21:47:03 -0500

    Updating Server.js

--- CoogWebBack/server.js
+++ CoogWebBack/server.js
@@ -1,12 +1,25 @@
-const http = require('http')
+const http = require('http');
+const url = require('url');
+const cors = require('cors');
+
+const corsMiddleWare = cors();
+
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<h1>Welcome to CoogWeb</h1>')
-    res.end()
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+    corsMiddleWare(req, res, () => {
+        const URL_PARSE = url.parse(req.url, true);
+        const {pathName} = URL_PARSE;
+        const method = req.method;
+    })
+
 })
-
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
