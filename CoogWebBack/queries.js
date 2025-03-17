d115896c1d3de2805996c9f63a61c3788f2cd314
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-03-16 23:04:47 -0500

    List Page Queries

--- CoogWebBack/queries.js
+++ CoogWebBack/queries.js
@@ -1,22 +1,38 @@
 // Query Inputs
 
 const getSongs = 
-    `SELECT * FROM SONG`;
+    `SELECT * FROM SONG;`
 
 const getUsers =
-    `SELECT * FROM USER`;
+    `SELECT * FROM USER;`
 
 const getArtists =
-    `SELECT * FROM ARTIST`;
+    `SELECT * FROM ARTIST;`
 
 const getAlbums = 
-    `SELECT * FROM ALBUM`;
+    `SELECT * FROM ALBUM;`
 
 const getPlaylists =
-    `SELECT * FROM PLAYLIST`;
+    `SELECT * FROM PLAYLIST;`
 
 const getSongListOutput =
-    `SELECT SONG.name, ARTIST.name, SONG.image_url
+    `SELECT SONG.name, ARTIST.name, SONG.image_url, SONG.song_url
      FROM SONG, ARTIST
-     WHERE SONG.artist_id = ARTIST.artist_id `;
+     WHERE SONG.artist_id = ARTIST.artist_id;`
+
+const getArtistListOutput =
+    `SELECT ARTIST.username, ARTIST.image_url
+     FROM ARTIST;`
+
+const getAlbumListOutput =
+    `SELECT ALBUM.name, ALBUM.image_url
+     FROM ALBUM;`
+
+const getPlaylistListOutput =
+    `SELECT PLAYLIST.name, PLAYLIST.image_url
+     FROM PLAYLIST;`
+    
+const getUserListOutput =
+    `SELECT USER.username, USER.image_url
+     FROM USER;`
 
