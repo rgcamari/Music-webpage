93e86bba3d2cdbdf617734927a00ca0f92d62c63
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-03-16 21:55:06 -0500

    Updating Server.js

--- CoogWebBack/server.js
+++ CoogWebBack/server.js
@@ -4,6 +4,17 @@ const cors = require('cors');
 
 const corsMiddleWare = cors();
 
+const map_route = {
+    'GET': [
+    ],
+    'POST': [
+    ],
+    'PUT': [
+    ],
+    'DELETE': [
+    ],
+};
+
 
 const server = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,6 +31,28 @@ const server = http.createServer((req, res) => {
         const URL_PARSE = url.parse(req.url, true);
         const {pathName} = URL_PARSE;
         const method = req.method;
-    })
 
+        if (pathName === "/") {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify("Backend"));
+            return;
+        }
+
+        const isMatch = (map_route[method] || []).some(route => pathName.startswith(route));
+
+        if (isMatch === true) {
+            return eRoutes(req,res);
+        }
+
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({error: "Route Not Found"}));
+    });
+});
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+    server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    })
 })
