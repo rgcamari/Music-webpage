9698b249c803a53fcccd4860b2f6b9f563c5c77c
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-03-17 22:39:22 -0500

    User vs. Artist Page Difference

--- CoogWeb/src/pages/home.js
+++ CoogWeb/src/pages/home.js
@@ -34,15 +34,21 @@ const TopBar = () => {
 };
 
 // SideBar Component
-const SideBar = ({ onButtonClick }) => {
+const SideBar = ({ onButtonClick, accountType }) => {
   return (
     <div className="side-bar">
       <button className="side-bar-button" onClick={() => onButtonClick('song-list')}>Song List</button>
       <button className="side-bar-button" onClick={() => onButtonClick('artist-list')}>Artist List</button>
       <button className="side-bar-button" onClick={() => onButtonClick('album-list')}>Album List</button>
-      <button className="side-bar-button" onClick={() => onButtonClick('user-lists')}>User List</button>
-      <button className="side-bar-button" onClick={() => onButtonClick('profile')}>Profile</button>
-      <button className="side-bar-button" onClick={() => onButtonClick('artist-profile')}>Artist Profile</button>
+      {accountType !== 'artist' && (
+        <button className="side-bar-button" onClick={() => onButtonClick('user-lists')}>User List</button>
+      )}
+      {accountType !== 'artist' && (
+        <button className="side-bar-button" onClick={() => onButtonClick('profile')}>Profile</button>
+      )}
+      {accountType !== 'user' && (
+        <button className="side-bar-button" onClick={() => onButtonClick('artist-profile')}>Artist Profile</button>
+      )}
       <button className="side-bar-button" onClick={() => onButtonClick('top-trending')}>Top Trending</button>
       <button className="side-bar-button" onClick={() => onButtonClick('cougar-wrap-up')}>Cougar Wrap-Up</button>
     </div>
@@ -137,7 +143,7 @@ const Home = () => {
     <div className="Home">
       <TopBar />
       <div className="content">
-        <SideBar onButtonClick={setActiveScreen} />
+        <SideBar onButtonClick={setActiveScreen} accountType = {'artist'} />
         <div className="main-content">
           {renderScreen(activeScreen, handleArtistClick, handleAlbumClick, handlePlaylistClick)}
         </div>
